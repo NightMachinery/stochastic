6b80e12a5bf3727e2d8c8dfcd0b2301ca8aa0b17
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-03 00:07:51 +0430

    .

--- exercises/c9.5a.jl
+++ exercises/c9.5a.jl
@@ -1,6 +1,14 @@
-function NPolar(u1, u2)
-    # u1 = rand()
-    # u2 = rand()
+
+# function stdN(u1, u2, sign1)
+#     while true
+#         x = -log(u1)
+#         if u2 <= (MathConstants.e^(((x - 1)^2) / -2))
+#             return x * sign1
+#         end
+#     end
+# end
+
+function NPolar_old(u1, u2)
     r = sqrt(-2 * log(u1))
     θ = 2 * pi * u2
     x = r * cos(θ)
@@ -8,12 +16,31 @@ function NPolar(u1, u2)
     return x, y
 end
 
+function NPolar(u1, u2, sign1)
+    # u1 = rand() # decreasing on u1
+    # u2 = rand() 
+    r = sqrt(-2 * log(u1))
+    θ = (pi / 2) * u2 # so decreasing on u2 as well
+    x = r * cos(θ) * sign1
+    # y = r * sin(θ)
+    return x
+end
+
 function antithetic_z()
+    u1 = rand()
+    u2 = rand()
+    sign1 = rand((-1, 1))
+    u1b = 1 - u1
+    u2b = 1 - u2
+    return [NPolar(u1, u2, sign1), NPolar(u1b, u2b, -sign1)]
+end
+
+function antithetic_z_old()
     u1 = rand()
     u2 = rand()
     u1b = 1 - u1
     u2b = 1 - u2
-    return [NPolar(u1, u2)..., NPolar(u1b, u2b)...]
+    return [NPolar_old(u1, u2)..., NPolar_old(u1b, u2b)...]
 end
 
 function Zorg(z)
@@ -21,18 +48,22 @@ function Zorg(z)
 end
 
 ##
-data_normals_anti = [z for i in 1:10^7 for z in antithetic_z()]
+n = 6
+
+data_normals_anti = [z for i in 1:(10^n * 2) for z in antithetic_z()]
 data_anti = [Zorg(z) for z in data_normals_anti]
 println("anti: mean=$(mean(data_anti)), var=$(std(data_anti; corrected=true)^2)")
 
-data_normals = [z for i in 1:(10^7 * 4) for z in rand(Normal())]
+data_normals_anti3 = [z for i in 1:10^n for z in antithetic_z_old()]
+data_anti3 = [Zorg(z) for z in data_normals_anti3]
+println("anti3: mean=$(mean(data_anti3)), var=$(std(data_anti3; corrected=true)^2)")
+
+
+data_normals = [z for i in 1:(10^n * 4) for z in rand(Normal())]
 data = [Zorg(z) for z in data_normals]
 println("Normal: mean=$(mean(data)), var=$(std(data; corrected=true)^2)")
 
-data_normals_anti2 = [z * sign for i in 1:(10^7 * 2) for z in rand(Normal()) for sign in (1, -1)]
+data_normals_anti2 = [z * sign for i in 1:(10^n * 2) for z in rand(Normal()) for sign in (1, -1)]
 data_anti2 = [Zorg(z) for z in data_normals_anti2]
 println("anti2: mean=$(mean(data_anti2)), var=$(std(data_anti2; corrected=true)^2)")
 
-# anti: mean=6.580210629998028, var=3521.696920529584
-# Normal: mean=6.598402309096034, var=3619.21725736029
-# anti2: mean=6.594510883285163, var=3586.564039420383
