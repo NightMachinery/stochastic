72109907ceab491c8bbfc8102cc5e2dbf53a7524
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-08-02 18:50:51 +0430

    .

--- common/plotSamples2.jl
+++ common/plotSamples2.jl
@@ -1,28 +1,28 @@
-using Colors, Gadfly, ColorSchemes, Distributions, Compose, UUIDs
+using Colors, Gadfly, ColorSchemes, Distributions, Compose, UUIDs, Printf
 import Cairo, Fontconfig
 
-function layerpdf(s ; color = RGBA(0, 1, 0, 0.7), line_width = 0.5mm, density = true, style_more...)
+function layerpdf(s ; color=RGBA(0, 1, 0, 0.7), line_width=0.5mm, density=true, style_more...)
     return layer(
-        x = s,
+        x=s,
         # Geom.histogram(density = density),
         Geom.line(),
-        Stat.histogram(density = density),
-        style( ; line_width = line_width, default_color = color, style_more...),
+        Stat.histogram(density=density),
+        style( ; line_width=line_width, default_color=color, style_more...),
     )
 end
-function layercdf(s ; color = RGBA(0, 1, 0, 0.7), line_width = 0.5mm, density = true, style_more...)
+function layercdf(s ; color=RGBA(0, 1, 0, 0.7), line_width=0.5mm, density=true, style_more...)
     n = length(s)
     ys = (1:n)
     if density
         ys = ys ./ n
     end
-    return layer(x = sort(s),
-        y = ys,
+    return layer(x=sort(s),
+        y=ys,
         Geom.line,
-        style( ; line_width = line_width, default_color = color, style_more...),
+        style( ; line_width=line_width, default_color=color, style_more...),
         )
 end
-function plotSamples(ss ; p_d = [], p_c = [], alpha_d = 0.7, alpha_c = 0.7 , colorscheme = ColorSchemes.gnuplot2, kwargs...)
+function plotSamples(ss ; p_d=[], p_c=[], alpha_d=0.7, alpha_c=0.7 , colorscheme=ColorSchemes.gnuplot2, kwargs...)
     n = length(ss) + 1 # no white color
     if (colorscheme isa Function)
         getcolor = colorscheme
@@ -31,34 +31,53 @@ function plotSamples(ss ; p_d = [], p_c = [], alpha_d = 0.7, alpha_c = 0.7 , col
             RGBA(get(colorscheme, i / n), alpha)
         end
     end
-    Ds = [layerpdf(s ; color = getcolor(i, n, alpha_d), kwargs...) for (i, s) in enumerate(ss)]
-    Cs = [layercdf(s ; color = getcolor(i, n, alpha_c), kwargs...) for (i, s) in enumerate(ss)]
+    Ds = [layerpdf(s ; color=getcolor(i, n, alpha_d), kwargs...) for (i, s) in enumerate(ss)]
+    Cs = [layercdf(s ; color=getcolor(i, n, alpha_c), kwargs...) for (i, s) in enumerate(ss)]
     p_d = plot(Ds..., p_d...)
     p_c = plot(Cs..., p_c...)
     return p_d, p_c
 end
 
-function drawSamples(E, V, λs = [1] ; n = 10^4, title1 = "Ours", title2 = "Theirs",  p_d = [], p_c = [], p_shared = [Guide.xlabel(""), Guide.ylabel("")] , kwargs...)
+function drawDistribution(E ; kwargs...)
+    # if we only draw E, the plot will be become too tall and ugly, so let's just run it twice.
+    # drawSamples((λ) -> E(), nothing)
+    drawSamples((λ) -> E(), (λ) -> E() ; kwargs...)
+
+    ## this didn't incease the details on the scales, but pressing `c` on the interactive graph shows exact coordinates so who cares.
+    # p_c=[Scale.x_continuous(minticks=6, labels=x -> @sprintf("%0.3f", x))]
+end
+
+"""
+E(λ) is our generator function of the distribution.
+V(λ) is the verifier generator function of the distribution.
+n is the number of samples to draw from them.
+λs are the lambdas with which to generate distributions.
+"""
+function drawSamples(E, V, λs=[1] ; n=10^4, title1="Ours", title2="Theirs",  p_d=[], p_c=[], p_shared=[Guide.xlabel(""), Guide.ylabel("")] , kwargs...)
     function getPlots(F) 
         return plotSamples([collect(Iterators.flatten([F(λ) for i in 1:n])) for λ in λs] ;
-            p_d = [
+            p_d=[
                 p_shared...,
                 p_d...
             ],
-            p_c = [
+            p_c=[
                 p_shared...,
                 p_c...
             ], kwargs...)
     end
     E_d, E_c = getPlots(E)
-    V_d, V_c = getPlots(V)
-
     ours = compose(context(0, 0, 1w, 0.4h), font("Jokerman"), fontsize(14pt), text(0.5, 1.0, title1, hcenter, vbottom))
-    theirs = compose(context(0, 0, 1w, 0.4h), font("Jokerman"), fontsize(14pt), text(0.5, 1.0, title2, hcenter, vbottom))
-    return (vstack(ours, hstack(E_d, E_c), theirs, hstack(V_d, V_c)))
+    out = [hstack(E_d, E_c)] 
+    if ! (V === nothing)
+       V_d, V_c = getPlots(V)
+       theirs = compose(context(0, 0, 1w, 0.4h), font("Jokerman"), fontsize(14pt), text(0.5, 1.0, title2, hcenter, vbottom))
+       out = [ours, out..., theirs, hstack(V_d, V_c)]
+    end
+
+    return (vstack(out...))
 end
 ##
-macro plot(cmd, format = "pngplus", prefix = "")
+macro plot(cmd, format="pngplus", prefix="")
     pltcmd = :(plt = $cmd)
     # cmdstr = replace(string(cmd), '\n' => " PANDA ")
     cmdstr = string(cmd)
@@ -91,7 +110,7 @@ macro plot(cmd, format = "pngplus", prefix = "")
             plt |> SVG(file)
             run(`open -a Safari $(file)`)
         elseif $format == "png"
-            plt |> PNG(file, dpi = 300)
+            plt |> PNG(file, dpi=300)
             run(`open -a Preview $(file)`)
             if $pngplus
                 file2 = "$(outdir)/$($prefix)$($cmdstr).auto.svg"
--- exercises/c5.2.jl
+++ exercises/c5.2.jl
@@ -1,3 +1,19 @@
 # We use the inverse method:
 # 0 < U < 1/4: X = 2√U + 2
-# 1/4 < U < 1: 
+# 1/4 < U < 1: X = 6(1 - Sqrt[1 - U]/Sqrt[3]) 
+## Mathematica calculations:
+# Solve[1 - 3/4 (2 - a/3)^2 == y, a]
+# {{a -> 6 (1 - Sqrt[1 - y]/Sqrt[3])}, {a -> 
+#    6 (1 + Sqrt[1 - y]/Sqrt[3])}} 
+
+function X_C5_2()
+    u = rand()
+    if 0 <= u <= 1 / 4
+        return 2sqrt(u) + 2
+    else
+        return 6(1 - sqrt(1 - u) / sqrt(3))
+    end
+end
+
+include("../common/plotSamples2.jl")
+drawDistribution(X_C5_2)
