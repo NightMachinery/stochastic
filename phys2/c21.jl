9899c7bd326ff8f214c3ab3a9946e528f2d989ac
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-02-28 12:04:34 +0330

    modified:   phys2/c21.jl

--- phys2/c21.jl
+++ phys2/c21.jl
@@ -1,6 +1,15 @@
 ## Useful functions
 # rad2deg
 ##
+using PhysicalConstants.CODATA2018
+# -
+using Unitful
+using ForceImport
+@force using Unitful.DefaultSymbols
+# https://painterqubits.github.io/Unitful.jl/stable/highlights/
+# 8m == 800cm
+# -
+##
 ϵ0 = 8.85 * 10 * -12
 e_k = 8.99 * 10^9
 FE(q1, q2, r)::Number = e_k * (abs(q1) * abs(q2) / abs2(r))
@@ -45,3 +54,33 @@ f3net = f31 + f32 + f34
 r_core = 4 * 10^-15
 fee = FE(q_e, q_e, r_core) 
 fee_g = FG(m_e, m_e, r_core)
+## P4_C1
+Mass = typeof(1.0u"kg")
+Speed = typeof(1.0u"m/s")
+MyDegree = typeof(1.0°)
+
+LinearMomentumScalar(mass::Mass, speed::Speed) = mass * speed
+lms = LinearMomentumScalar
+LinearMomentumVector(mass::Mass, speed::Vector{Speed}) = mass * speed
+lmv = LinearMomentumVector
+function vecθ(length, θ)
+	# No general degree dimension, so leave it untyped to dispatch automatically
+	[length * cos(θ), length * sin(θ)]
+end
+function vec2θ(v)
+	return mag2(v), uconvert(°, atan(v[2], v[1])u"rad")
+end
+## e1.1
+he_m = (6.6465 * 10.0^-27)kg
+he_v = 1.518e6u"m/s"
+he_V = vecθ(he_v, 0)
+ni_m = 2.3253e-26kg
+ni_V = vecθ(0.0u"m/s", 0)
+he_v2 = 1.199e6u"m/s"
+he_a = 78.75°
+he_V2 = vecθ(he_v2, he_a)
+#-
+ni_V2 = (lmv(he_m,he_V) - (lmv(he_m,he_V2)))/ni_m
+@labeled ni_V2
+@labeled vec2θ(ni_V2)
+##
