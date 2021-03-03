15824c42ec8cc6fdc56081dc437e29aecd45bead
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2021-03-03 17:01:50 +0330

    modified:   phys2/c21.jl; new file:   phys2/old.jl

--- phys2/c21.jl
+++ phys2/c21.jl
@@ -1,8 +1,9 @@
 ## Useful functions
 # rad2deg
 ##
+(@isdefined SunHasSet) || begin include("../common/startup.jl") ; println("Using backup startup.jl.") end
+using LinearAlgebra
 using PhysicalConstants.CODATA2018
-# -
 using Unitful
 using ForceImport
 @force using Unitful.DefaultSymbols
@@ -10,65 +11,101 @@ using ForceImport
 # 8m == 800cm
 # -
 ##
-ϵ0 = 8.85 * 10 * -12
-e_k = 8.99 * 10^9
-FE(q1, q2, r)::Number = e_k * (abs(q1) * abs(q2) / abs2(r))
-q_e = 1.6 * 10^-19
-#
-g_k = 6.67 * 10^-11
-FG(m1, m2, r) = g_k * (m1 * m2 / abs2(r))
-m_e = 1.67 * 10^-27
-#
-mag2(p) = sqrt(abs2(p[1]) + abs2(p[2]))
-function FV(forceFn, p1, p2)
-	r = mag2(p2 - p1)
-	m1 = p1[3]
-	m2 = p2[3]
-	force = forceFn(m1, m2, r)
-	angle = atan(p2[2] - p1[2], p2[1] - p1[1]) # so this force is from p1 towards p2
-	# angle = atan(p1[2] - p2[2], p1[1] - p2[1]) # so this force is from p2 towards p1
-	@labeled (angle |> rad2deg) 
-	@labeled force * cos(angle)
-	@labeled force * sin(angle)
-	return [force * cos(angle), force * sin(angle)] * sign(m1) * sign(m2)
-end
-function fnMul(f, v)
-	function f2(args... ; kargs...)
-		return v * f(args... ; kargs...)
-	end
-	return f2
-end
-FVE(args...) = FV(fnMul(FE, -1), args...)
-## 
-# c21.p11:
-a = 0.05
-q1 = [0, a, 100 * 10^-9]
-q2 = [a, a, -100 * 10^-9]
-q3 = [0, 0, 200 * 10^-9]
-q4 = [a, 0, -200 * 10^-9]
-f31 = FVE(q3, q1)
-f32 = FVE(q3, q2)
-f34 = FVE(q3, q4)
-f3net = f31 + f32 + f34
+# julia> using Unitful, Latexify, UnitfulLatexify; # this last one is new
+# julia> latexify(24u"km")
+# L"$24\;\mathrm{km}$"
+# julia> latexify(24u"km",unitformat=:siunitx)
+# L"\SI{24}{\kilo\meter}"
 ##
-r_core = 4 * 10^-15
-fee = FE(q_e, q_e, r_core) 
-fee_g = FG(m_e, m_e, r_core)
+using Formatting
+
+function shift_unit(u::U, d, n) where {U<:Unitful.Unit}
+    i₀ = floor(Int, d/n)
+    d = 3i₀
+    iszero(d) && return u,0
+    for (i,tens) in enumerate(Unitful.tens(u) .+ (d:(-3*sign(d)):0))
+        haskey(Unitful.prefixdict, tens) && return U(tens, u.power),i-1 + i₀
+    end
+    u,0
+end
+
+power_step(::Any) = 3
+
+function shift_unit(u::Unitful.FreeUnits, d)
+    tu = typeof(u)
+    us,ds,a = tu.parameters
+
+    uu,i = shift_unit(us[1], d, power_step(1u))
+
+    Unitful.FreeUnits{(uu,us[2:end]...), ds, a}(),i
+end
+
+logu(::Any, v) = log10(v)
+divider(::Any) = 1.0
+
+function si_round(q::Quantity; fspec="{1:+9.4f} {2:s}")
+    v,u = ustrip(q), unit(q)
+    if !iszero(v)
+        u,i = shift_unit(u, logu(q,abs(v)))
+        q = u(q/(divider(q)^i))
+    end
+    format(fspec, ustrip(q), unit(q))
+end
+ss = si_round
+##
 ## P4_C1
 Mass = typeof(1.0u"kg")
 Speed = typeof(1.0u"m/s")
+Length = typeof(1.0u"m")
 MyDegree = typeof(1.0°)
-
-LinearMomentumScalar(mass::Mass, speed::Speed) = mass * speed
-lms = LinearMomentumScalar
-LinearMomentumVector(mass::Mass, speed::Vector{Speed}) = mass * speed
-lmv = LinearMomentumVector
+Momentum = typeof(1.0u"kg*m/s")
+Energy = typeof(1.0u"J")
+Charge = typeof(1.0u"C")
+Force = typeof(1.0u"N")
+Base.@kwdef mutable struct Particle 
+    position::Vector{Length} = [0.0u"m", 0.0u"m"]
+    mass::Mass = 0.0u"kg"
+    charge::Charge = 0.0u"C"
+    speed::Vector{Speed} = [0.0u"m/s", 0.0u"m/s"]
+end
+##
+LinearMomentum(mass::Mass, speed::Speed) = mass * speed
+LinearMomentum(mass::Mass, speed::Vector{Speed}) = mass * speed
+lmom = LinearMomentum
+AngularMomentum(position::Vector{Length}, momentum::Momentum) = cross2(position, momentum)
+##
+ElectricConstant = 1 / (4 * π * VacuumElectricPermittivity)
+ElectricForce(charge1::Charge, charge2::Charge, r::Length)::Force = (ElectricConstant) * abs(charge1) * abs(charge2) / abs2(r)
+ElectricForce(charge1::Charge, charge2::Charge, r::Vector{Length})::Vector{Force} = (ElectricConstant) * charge1 * charge2 * r / norm(r)^3
+function ElectricForce(p1::Particle, p2::Particle)::Vector{Force}
+    ElectronMass(p1.charge, p2.charge, p1.position - p2.position)
+end
+##
+KineticEnergy(m::Mass, v::Speed)::Energy = 0.5 * m * v^2
+KineticEnergy(m::Mass, p::Momentum)::Energy = 0.5 * p^2 / m
+KineticEnergy(m::Mass, v::Vector)::Energy = KineticEnergy(m, mag2(v))
+ke = KineticEnergy
+# tests:
+# KineticEnergy(4.0kg,[1.0m/s,1.0m/s])
+# KineticEnergy(4.0kg,[4.0kg*m/s,4.0kg*m/s])
+##
 function vecθ(length, θ)
-	# No general degree dimension, so leave it untyped to dispatch automatically
-	[length * cos(θ), length * sin(θ)]
+    # No general degree dimension, so leave it untyped to dispatch automatically
+    [length * cos(θ), length * sin(θ)]
 end
 function vec2θ(v)
-	return mag2(v), uconvert(°, atan(v[2], v[1])u"rad")
+    return mag2(v), uconvert(°, atan(v[2], v[1])u"rad")
+end
+mag2(p) = norm(p) # sqrt(abs2(p[1]) + abs2(p[2]))
+function cross2(v1, v2)
+    # test: `cross2([1m,0m], [1kg*m/s,1kg*m/s])`
+    if length(v1) == 2
+        v1 = [v1[1], v1[2], v1[1] * 0]
+    end
+    if length(v2) == 2
+        v2 = [v2[1], v2[2], v2[1] * 0]
+    end
+    cross(v1, v2)
 end
 ## e1.1
 he_m = (6.6465 * 10.0^-27)kg
@@ -79,8 +116,25 @@ ni_V = vecθ(0.0u"m/s", 0)
 he_v2 = 1.199e6u"m/s"
 he_a = 78.75°
 he_V2 = vecθ(he_v2, he_a)
-#-
-ni_V2 = (lmv(he_m,he_V) - (lmv(he_m,he_V2)))/ni_m
+# -
+ni_V2 = (lmom(he_m, he_V) - (lmom(he_m, he_V2))) / ni_m
+##
+cm_V2 = (he_V2 * he_m + ni_V2 * ni_m) / (he_m + ni_m)
+he_cm_V2 = he_V2 - cm_V2
+@labeled vec2θ(he_cm_V2)
+@labeled ni_cm_V2 = ni_V2 - cm_V2
+@labeled vec2θ(ni_cm_V2)
+##
 @labeled ni_V2
 @labeled vec2θ(ni_V2)
+@labeled k1 = ke(he_m, he_V)
+@labeled k2 = ke(he_m, he_V2) + ke(ni_m, ni_V2)
+@labeled k2 - k1
+## e1.2
+ur_m = 3.9529e-25kg
+tho_m = 3.8864e-25kg
+he_V = vecθ(1.423e7m / s, 0)
+ni_V = -he_V * he_m / tho_m
+@labeled vec2θ(ni_V)
+@labeled ke(ni_m, ni_V) + ke(he_m, he_V)
 ##
